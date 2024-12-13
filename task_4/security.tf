b44a0fd915405216700022bb57e76a491ac818e5
Tohid Hanfi <119034342+tohidhanfi20@users.noreply.github.com>
2024-12-13 18:55:03 +0530

    Update security.tf

--- task_4/security.tf
+++ task_4/security.tf
@@ -1,1 +1,22 @@
 
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_traffic"
+  description = "Security group will allows all inbound and outbound traffic"
+
+  
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # -1 means all protocols
+    cidr_blocks  = ["0.0.0.0/0"]  # Allow from anywhere
+  }
+
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # -1 means all protocols
+    cidr_blocks  = ["0.0.0.0/0"]  # Allow to anywhere
+  }
+}
+
